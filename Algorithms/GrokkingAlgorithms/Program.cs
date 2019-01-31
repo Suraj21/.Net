181ba5989041f3e1759da0d26a66375aad1677f4
sachidanand suraj <suraj.thepions@gmail.com>
2019-01-31 09:46:22 +0530

    Quick Sort  Implementation
    Quick Sort  Implementation

--- Algorithms/GrokkingAlgorithms/Program.cs
+++ Algorithms/GrokkingAlgorithms/Program.cs
@@ -10,6 +10,32 @@ namespace GrokkingAlgorithms
             Console.WriteLine("Binary Search Algorithm");
             List<int> no_List = new List<int> { 1, 2, 4, 5, 6, 8, 9, 11, 13 };
             object result = BinarySearch.Binary_Search(no_List, 5);
+            Console.WriteLine("Binary Search Result: " + result);
+
+            no_List = new List<int> { 5, 4, 3, 6 };
+
+            Console.WriteLine("no_List being passed to Sort result:");
+            foreach (var item in no_List)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] selectionSortResult = SelectionSort.Selection_Sort(no_List.ToArray());
+
+            Console.WriteLine("Selection Sort Result: \n");
+            for (int i = 0; i < selectionSortResult.Length; i++)
+            {
+                Console.WriteLine(selectionSortResult[i]);
+            }
+
+            List<int> quickSortResult = QuickSort.QuickSortMethod(no_List);
+            Console.WriteLine("Quick Sort Result: \n");
+            foreach (var item in quickSortResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Algorithms/GrokkingAlgorithms/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrokkingAlgorithms
+{
+    public class QuickSort
+    {
+        public static List<int> QuickSortMethod(List<int> array)
+        {
+            List<int> result = new List<int>();
+            if(array.Count < 2)
+            {
+                result = array.OfType<int>().ToList();
+                return result;
+            }
+            else
+            {
+                int pivot = array[0]; // Recursive Array
+                List<int> less = new List<int>(array.Count);
+                List<int> greater = new List<int>(array.Count);
+
+                int counter = 0;
+                for (int i=1; i < array.Count; i++)
+                {
+                    if(array[i] <= pivot)
+                    {
+                        less.Add(array[i]);
+                        counter++;
+                    }
+                }
+
+                counter = 0;
+                for (int i = 1; i < array.Count; i++)
+                {
+                    if(array[i] > pivot)
+                    {
+                        greater.Add(array[i]);
+                        counter++;
+                    }
+                }
+                result.AddRange(less);
+                result.Add(pivot);
+                result.AddRange(greater);
+                if (less.Count > 0)
+                {
+                    result = QuickSortMethod(result);
+                }
+                return result;
+            }
+        }
+    }
+}
