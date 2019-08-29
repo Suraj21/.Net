83be9d380e4c2be2f0b80e6b23dc1475728f1f79
Sachidanand Suraj <suraj.thepions@gmail.com>
2019-08-29 16:24:31 +0530

    Added views

---
+++ TaskManagementApp/Views/CurrentTasksView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using TaskManagementApp.ViewModel;
+
+namespace TaskManagementApp.Views
+{
+    /// <summary>
+    /// Interaction logic for CurrentTasks.xaml
+    /// </summary>
+    public partial class CurrentTasksView : UserControl
+    {
+        public CurrentTasksView()
+        {
+            InitializeComponent();
+            this.DataContext = new CurrentTasksViewModel();
+        }
+    }
+}
