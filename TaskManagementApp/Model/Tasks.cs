196716e1137f387c242d541986d7f66a7fdec7bd
Sachidanand Suraj <suraj.thepions@gmail.com>
2019-08-29 16:27:19 +0530

    Model file

---
+++ TaskManagementApp/Model/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskManagementApp.Model
+{
+    class Tasks
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string StartDate { get; set; }
+        public string DueDate { get; set; }
+        public string DateAdded { get; set; }
+    }
+}
