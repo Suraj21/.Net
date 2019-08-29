2562b838eec567411ef963316f121c1380b1e579
Sachidanand Suraj <suraj.thepions@gmail.com>
2019-08-29 16:25:01 +0530

    ViewModel Base file

---
+++ TaskManagementApp/ViewModelBase/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace TaskManagementApp
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
