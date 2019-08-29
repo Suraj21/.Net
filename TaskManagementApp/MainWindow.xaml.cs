6c410ce8a93e07df5fdaf2d682122f88ec981295
Sachidanand Suraj <suraj.thepions@gmail.com>
2019-08-29 16:23:54 +0530

    Added Task Management Application
    Added Task Management Application

---
+++ TaskManagementApp/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskManagementApp.Views;
+
+namespace TaskManagementApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            UserControl usc = new CurrentTasksView();
+            ContentGrid.Children.Add(usc);
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserControl usc = null;
+            ContentGrid.Children.Clear();
+
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "ItemCurrentTask":
+                    usc = new CurrentTasksView();
+                    ContentGrid.Children.Add(usc);
+                    break;
+                case "ItemAllTasks":
+                    usc = new AllTasks();
+                    ContentGrid.Children.Add(usc);
+                    break;
+                case "ItemCalender":
+                    usc = new CalenderView();
+                    ContentGrid.Children.Add(usc);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
